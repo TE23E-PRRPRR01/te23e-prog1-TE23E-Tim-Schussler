a31ea2fa14e942aacbb3960f2735429e96d22877
Tim Schüssler <tim.schussler@elev.ga.ntig.se>
2024-08-26 14:31:54 +0200

    Agsahpojshrp

---
+++ Kapitel-1/Registrering/Program.cs
@@ -0,0 +1,15 @@
+﻿// Program för att registrera förnamn och efternamn...
+Console.Clear();
+Console.InputEncoding = System.Text.Encoding.Unicode;
+Console.OutputEncoding = System.Text.Encoding.Unicode;
+Console.WriteLine("Program för att registrera förnamn och efternamn...");
+Console.WriteLine("Ange förnamn");
+string förnamn = Console.ReadLine();
+Console.WriteLine("Ange efternamn");
+string efternamn = Console.ReadLine();
+Console.WriteLine("Välkommen " + förnamn + " " + efternamn);
+Console.WriteLine("█░███░█");
+Console.WriteLine("███████");
+Console.WriteLine("█░███░█");
+Console.WriteLine("█░░░░░█");
+ 
