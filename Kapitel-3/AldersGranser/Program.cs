b2adbb73ffee2c7ba75e3665cb65f9b2c3dd4597
Tim Schüssler <tim.schussler@elev.ga.ntig.se>
2024-09-09 14:53:19 +0200

    Integer variabler

---
+++ Kapitel-3/AldersGranser/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Clear();
+Console.Write("Hur gammal var du när du först såg en babian? ");
+String Age = Console.ReadLine();
+int ålder = int.Parse(Age);
+if (ålder <= 2) {
+    Console.WriteLine("Du är nog en babian själv");
+} else {
+   if (ålder <= 9) {
+    Console.WriteLine("Du måste ha varit imponerad");
+} else {
+    if (ålder <= 18) {
+    Console.WriteLine("Nu var det nog inte lika intressant");
+} else {
+    if (ålder <= 50) {
+    Console.WriteLine("Du gjorde det nog av egen vilja");
+} else {
+    Console.WriteLine("Du kan vara glad att du hann se en!");
+}
+}
+} 
+}
+
+
---
+++ Kapitel-3/GissaTalet/Program.cs
@@ -0,0 +1,27 @@
+﻿// GISSA HELTALET!!!!!!
+using System.Net.Http.Headers;
+
+
+//Slumpa ett tal
+int slumptalet = Random.Shared.Next(1, 1001);
+retry:
+Console.WriteLine("Guess the secret number between 1-1000");
+Console.Write("Guess: ");
+//Läs in texten, t.ex "100"
+string gissningText = Console.ReadLine();
+//Konvertera texten till ett heltal, "100" blir 100
+int gissning = int.Parse(gissningText);
+Console.Clear();
+if (gissning == slumptalet)
+{
+    Console.WriteLine("Congratulations!");
+} else {
+if (gissning >= slumptalet)
+{
+    Console.WriteLine("Too high");
+    goto retry;
+} else {
+Console.WriteLine("Too low");
+goto retry;
+}
+}
