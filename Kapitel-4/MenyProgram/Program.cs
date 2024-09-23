ba11d1b8fb728f162237e0b323a01221e7d24011
Tim Schüssler <tim.schussler@elev.ga.ntig.se>
2024-09-23 14:49:31 +0200

    while (true) loopar och hantering av txt filer

---
+++ Kapitel-4/MenyProgram/Program.cs
@@ -0,0 +1,62 @@
+﻿// Ett menyprogram
+Console.Clear();
+Console.InputEncoding = System.Text.Encoding.Unicode;
+Console.OutputEncoding = System.Text.Encoding.Unicode;
+// Val variabel
+string val = "";
+// En loop
+while (true)
+{
+    // Visar meny och läser in dina val
+    Console.WriteLine("""
+    ╓────────────────────╖
+    ║Välj ett alternativ:║
+    ╙───╥────────────────╨──────────────╖
+        ║1.Omvandla text till versaler  ║
+        ║2.Omvandla en text till gemener║
+        ║3.Avsluta                      ║
+        ╙───────────────────────────────╜
+    """);
+    val = Console.ReadLine();
+    // Konditioner
+    if (val == "1")
+    {
+        Console.WriteLine("""
+
+           ╓────────────────────╖
+           ║ Text till Versaler ║
+           ║ Ange en mening     ║
+           ╙────────────────────╜
+        """);
+        string texten = Console.ReadLine().ToUpper();
+        Console.WriteLine($"Resultat : {texten}");
+    } else if (val == "2")
+    {
+                Console.WriteLine("""
+
+           ╓────────────────────╖
+           ║ Text till Gemener  ║
+           ║ Ange en mening     ║
+           ╙────────────────────╜
+        """);
+        string texten = Console.ReadLine().ToUpper();
+        Console.WriteLine($"Resultat : {texten}");
+    } else if (val == "3")
+    {
+        Console.WriteLine("""
+
+           ╓────────────────────╖
+           ║ Exit               ║
+           ║ Avslutar...        ║
+           ╙────────────────────╜
+        """);
+    } else {
+        Console.WriteLine("""
+
+           ╓────────────────────╖
+           ║ ERROR         024  ║
+           ║ Insufficient Input ║
+           ╙────────────────────╜
+        """);
+    }
+}
---
+++ Kapitel-4/TextEditor/Program.cs
@@ -0,0 +1,78 @@
+﻿// Ett program för att redigera och spara textfiler
+Console.Clear();
+Console.InputEncoding = System.Text.Encoding.Unicode;
+Console.OutputEncoding = System.Text.Encoding.Unicode;
+string choice = "";
+while (true){
+    Console.WriteLine("""
+    ╔═════════════════╗
+    ║Tim's Text Editor║
+    ╠═════════════════╣
+    ║1. Write new file║
+    ║2. Read from file║
+    ║3. Exit          ║
+    ╚═════════════════╝
+      ENTER NUMBER
+    """);
+      Console.Write(" : ");
+    choice = Console.ReadLine();
+    //Konditioner
+    if (choice == "1"){
+        Console.Clear();
+            Console.WriteLine("""
+    ╔═════════════════╗
+    ║Enter text below ║
+    ╠═════════════════╩════════════════════════════════════════════════════════╗
+    """);
+    Console.Write(" : ");
+    string text = Console.ReadLine();
+                            Console.WriteLine("""
+    ║Enter Textfile Name  ║
+    ╠═════════════════════╩════════════════════════════════════════════════════╣
+    """);
+      Console.Write(" : ");
+    string adress = Console.ReadLine();
+         File.WriteAllText(adress, text);
+    } else if (choice == "2") {
+        Console.Clear();
+                    Console.WriteLine("""
+    ╔═════════════════════════╗
+    ║Enter Textfile Name      ║
+    ╠═════════════════════════╩════════════════════════════════════════════════╗
+    """);
+      Console.Write(" : ");
+        string adress = Console.ReadLine();
+                      if (File.Exists(adress)){
+                                       Console.WriteLine("""
+    ╠══════════════════════════════════════════════════════════════════════════╣
+    """);
+      Console.Write(" : ");
+        string textfile = File.ReadAllText(adress);
+        Console.WriteLine(textfile);
+        } else {
+                    Console.Clear();
+                                    Console.WriteLine("""
+    ╔═════════════════════════╗
+    ║ERROR 002                ║
+    ║File does not Exist!     ║
+    ╚═════════════════════════╝
+    """);
+        }
+  
+    } else if (choice == "3") {
+        Console.Clear();
+                            Console.WriteLine("""
+    ╔═════════════════════════╗
+    ║Exitting Program...      ║
+    ╚═════════════════════════╝
+    """);
+    break;
+    } else {
+        Console.Clear();
+                                    Console.WriteLine("""
+    ╔═════════════════════════╗
+    ║ERROR 001                ║
+    ╚═════════════════════════╝
+    """);
+    }
+}
