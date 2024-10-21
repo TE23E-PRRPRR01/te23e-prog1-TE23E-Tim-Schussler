da3afd52b65fc476e3d8351858d5837c41c554f3
Tim Schüssler <tim.schussler@elev.ga.ntig.se>
2024-10-21 14:51:44 +0200

    Använt Index och Loopar för att göra ett "CaesarSchiffer"

---
+++ Kapitel-5/CeasarKrypto/Program.cs
@@ -0,0 +1,37 @@
+﻿// Ett program med kryptering med Caesar-Schiffer
+Console.Clear();
+Console.InputEncoding = System.Text.Encoding.Unicode;
+Console.WriteLine("Ett program för kryptering med Caesar-Schiffer");
+
+// Ange nyckel
+Console.Write("Ange krypteringsnyckel (1-9): ");
+int nyckel = int.Parse(Console.ReadLine());
+
+// Alfabetet
+string alfabetet = "ABCDEFGHIJKLMNOPQRSTUVWXYZÅÄÖ";
+ 
+// Ange en bokstav
+Console.Write("Ange ett meddelande: ");
+string meddelande = Console.ReadLine();
+Console.Clear();
+Console.Write("Meddelande: ");
+int nyIndex = 0;
+foreach (char bokstav in meddelande)
+{
+ // Hitta bokstavs position
+ int index = alfabetet.IndexOf(bokstav);
+ if (index == -1) {
+     Console.Write(bokstav);
+ } else {
+     // Caesar-kryptering, addera en nyckel (t.ex två)
+     nyIndex = index + nyckel;
+     if (nyIndex >= 28){
+        nyIndex = nyIndex - 29;
+     }
+ 
+     // Plocka ur bokstaven för nyIndex
+     char krypteradBokstav = alfabetet[nyIndex];
+     Console.Write(krypteradBokstav);
+ }   
+}
+
