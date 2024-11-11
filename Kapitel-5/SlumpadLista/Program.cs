057716147e5257f64d3e9b5f868f9578218fc9d7
Tim Schüssler <tim.schussler@elev.ga.ntig.se>
2024-11-11 14:53:51 +0100

    Använt metoder

---
+++ Kapitel-5/SlumpadLista/Program.cs
@@ -0,0 +1,20 @@
+﻿// Slumpa många tal och spara i lista
+Console.Clear();
+Console.WriteLine("Slumpa många tal och spara i lista");
+List<int> listaSlumptal = [];
+// Ange antal
+Console.Write("Hur många tal?: ");
+int antal = int.Parse(Console.ReadLine());
+Console.Write("Minimumtal?: ");
+int min = int.Parse(Console.ReadLine());
+Console.Write("Maximumtal?: ");
+int max = int.Parse(Console.ReadLine());
+// Loopa 5 ggr
+for (var i = 0; i < antal; i++)
+{
+    // Slumpa ett tal 1-100
+    int slumptal = 0;
+    slumptal = Random.Shared.Next(min, max);
+    Console.WriteLine($"Slumpat tal {i + 1}: {slumptal}");
+    listaSlumptal.Add(slumptal);
+}
---
+++ Kapitel-5/SlumpadLista2.0/Program.cs
@@ -0,0 +1,51 @@
+﻿// Lista med slumpmässiga tal
+
+/* ****************************************************************** 
+**                            Main                                 **
+******************************************************************* */
+
+// Validera inmatningen
+using System.Diagnostics;
+
+Console.Clear();
+ 
+// Deklarera variabler
+// Skapa en tom lista för att spara slumpade tal
+List<int> listaSlumptal = [];
+int antal = LäsInHeltal();
+int max = LäsInHeltal();
+int min = LäsInHeltal();
+
+for (var i = 0; i < antal; i++)
+{
+    // Slumpa ett tal 1-100
+    int slumptal = 0;
+    slumptal = Random.Shared.Next(min, max);
+    Console.WriteLine($"Slumpat tal {i + 1}: {slumptal}");
+    listaSlumptal.Add(slumptal);
+}
+
+/* ****************************************************************** 
+**                      Mina egna metoder                          **
+******************************************************************* */
+
+static int LäsInHeltal()
+{
+    int heltal = 0;
+while(true){
+    // Fråga användaren hur många slumptal vi ska ha
+    Console.Write("Ange heltal: ");
+
+    bool lyckades = int.TryParse(Console.ReadLine(), out heltal);
+
+    if(lyckades)
+    {
+       break;
+    }
+    else
+    {
+       Console.WriteLine("Du måste skriva ett heltal");
+    }
+}
+return heltal;
+}
